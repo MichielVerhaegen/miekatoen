2d5bd1c5a83d4ebe31a7b817dcb813f8c636cf18
RobbeVerhelst9 <verhelstrobbe1@gmail.com>
2021-10-15 17:09:42 +0200

    Navbar

--- miekatoen/src/components/navbar.jsx
+++ miekatoen/src/components/navbar.jsx
@@ -1,11 +1,17 @@
+import Logo from './img/logo.png'
+
 export function NavBar(){
   return(
-    <nav className="bg-roze ">
-      <a href="">Home</a>
-      <a href="">Poppen</a>
-      <a href="">About</a>
-      <a href="">Contact </a>
-      <a href="">Bestellen</a>
+    <nav className="bg-white flex inline-block border-b-4 border-bordeau">
+      <img src={Logo} alt="Logo" className="bg-white object-contain w-20 m-5"></img>
+      <div className="text-2xl my-auto">
+        <a href="" className="mx-10">Home</a>
+        <a href="" className="mx-10">Poppen</a> 
+        <a href="" className="mx-10">About</a>
+        <a href="" className="mx-10">Contact </a>
+        <a href="" className="mx-10">Bestellen</a>
+      </div>
+      
     </nav>
   )
 }
