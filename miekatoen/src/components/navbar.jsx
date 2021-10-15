e96599f6120b8ed77f81968b163cdb6b7457d4e0
RobbeVerhelst9 <verhelstrobbe1@gmail.com>
2021-10-15 17:54:33 +0200

    NavBar + White Logo

--- miekatoen/src/components/navbar.jsx
+++ miekatoen/src/components/navbar.jsx
@@ -1,16 +1,16 @@
-import Logo from './img/logo.png'
+import Logo from './img/logo-white.png'
 
 export function NavBar(){
   return(
-    <nav className="bg-white flex inline-block border-b-4 border-bordeau">
+    <nav className="bg-roze flex inline-block border-b-4 border-bordeau">
       <img src={Logo} alt="Logo" className="object-contain w-20 m-2"></img>
-      <div className="text-2xl my-auto">
+      <div className="text-3xl text-bordeau my-auto ">
         <a href="/" className="px-10 border-r-2 border-bordeau">Home</a>
         <a href="/" className="px-10 border-r-2 border-bordeau">Poppen</a> 
         <a href="/" className="px-10 border-r-2 border-bordeau">About</a>
-        <a href="/" className="px-10 border-bordeau">Contact </a>
+        <a href="/" className="px-10 ">Contact </a>
       </div>
-      
+
     </nav>
   )
 }
