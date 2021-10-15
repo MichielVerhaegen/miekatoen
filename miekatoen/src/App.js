5c50989d53077dd9887a47fc17f419a63717af46
michiel verhaegen <79313059+MichielVerhaegen@users.noreply.github.com>
2021-10-15 16:38:33 +0200

    homepage

--- miekatoen/src/App.js
+++ miekatoen/src/App.js
@@ -1,10 +1,12 @@
 import { NavBar } from './components/navbar';
+import { Homepage } from './components/Homepage';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Homepage />
     </div>
   );
 }
---
+++ miekatoen/src/components/Homepage.jsx
@@ -0,0 +1,13 @@
+export function Homepage(){
+return(
+  <body className="bg-cream">
+    <article className="max-w-full p-4">
+      <h1>
+
+      </h1>
+      <img src="" alt="" />
+      <p></p>
+    </article>
+  </body>
+)
+}
