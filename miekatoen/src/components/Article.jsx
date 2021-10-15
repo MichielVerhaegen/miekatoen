cdc842e7491e70239277a29373b048f8d2e57de3
michiel verhaegen <79313059+MichielVerhaegen@users.noreply.github.com>
2021-10-15 18:31:48 +0200

    foto's inorde gekregen
    yes foto'in orde

--- miekatoen/src/components/Article.jsx
+++ miekatoen/src/components/Article.jsx
@@ -1,14 +1,15 @@
-
+import React from 'react'
 export function Article(article){
   
-  const image = require(article.imgurl);
+
+ 
 return(
-  <article className="p-8 border-2 border-b-4 border-bordeau max-w-full m-4 rounded-xl">
-  <h1>
+  <article className="p-8 border-2 border-b-4 border-bordeau max-w-full m-4 rounded-xl bg-roze">
+  <h1 className=" text-bordeau text-xl font-bold">
    {article.titel}
   </h1>  
-  
-  return(<img src={image} alt="image of dolls" className='max-w-sm rotate-90'/>)
+  <img src={require(`${article.imgurl}`).default} alt="image of dolls" className='max-w-sm rotate-90 h-1/2 w-full object-cover '/>
+   
 
   <p>{article.description}</p>
 </article>
@@ -16,6 +17,3 @@ return(
 
 }
 
-function picture(){
-  return 'hallllo'
-}
