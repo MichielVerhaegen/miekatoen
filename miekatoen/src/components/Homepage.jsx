9e037be2140b3982e1605d1da451894b03c73b3d
michiel verhaegen <79313059+MichielVerhaegen@users.noreply.github.com>
2021-10-15 17:10:30 +0200

    push article

--- miekatoen/src/components/Homepage.jsx
+++ miekatoen/src/components/Homepage.jsx
@@ -1,13 +1,17 @@
 import Logo from './img/logo.png'
+import Homepagebanner from './img/homepagebanner.jpg'
 export function Homepage(){
 return(
   <body className="bg-cream flex">
-    <article className="p-8 border-2 border-b-4 border-bordeau max-w-full m-8 rounded-xl">
+    <article className="p-8 border-2 border-b-4 border-bordeau max-w-full m-60 rounded-xl">
       <h1>
         Poppen met een verhaal van Atelier Miekatoen
       </h1>
-      <img src={Logo} alt="image of dolls" />
-      <p></p>
+      <img src={Homepagebanner} alt="image of dolls" className='max-w-sm rotate-90'/>
+      <p>Atelier Mie Katoen is een poppenatelier gespecialiseerd in handgemaakte, 
+        poppen en poppenkleding. De poppen zijn uniek, genderneutraal en hebben 
+        elk hun eigen verhaal. Het atelier kiest ervoor om poppen te maken vanuit
+         een brede diversiteit, elk met zijn tekortkomingen en talenten.</p>
     </article>
   </body>
 )
