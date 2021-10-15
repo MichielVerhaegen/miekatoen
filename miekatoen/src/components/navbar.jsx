cbd5c7976dc22b1413e109bfbf9946e89e143f06
RobbeVerhelst9 <verhelstrobbe1@gmail.com>
2021-10-15 16:39:30 +0200

    navbar

--- miekatoen/src/components/navbar.jsx
+++ miekatoen/src/components/navbar.jsx
@@ -1,7 +1,11 @@
 export function NavBar(){
   return(
-    <nav className="bg-roze">
-      
+    <nav className="bg-roze ">
+      <a href="">Home</a>
+      <a href="">Poppen</a>
+      <a href="">About</a>
+      <a href="">Contact </a>
+      <a href="">Bestellen</a>
     </nav>
   )
 }
